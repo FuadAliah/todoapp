aa9dba8580243f6d9442759eb910f24232dfbd81
Fuad Aliah <eye.design17@yahoo.com>
2021-05-04 00:26:50 +0300

    Updates

---
+++ precache-manifest.9875cc83a073abbd31b26ad91af3b636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5aed375ee3efb7a122e",
+    "url": "/Todo/static/css/main.021cfc3d.chunk.css"
+  },
+  {
+    "revision": "b5aed375ee3efb7a122e",
+    "url": "/Todo/static/js/main.111610f3.chunk.js"
+  },
+  {
+    "revision": "6fab73d75525f9d6064a",
+    "url": "/Todo/static/js/runtime~main.ee49a031.js"
+  },
+  {
+    "revision": "c98c98277484661a3f51",
+    "url": "/Todo/static/js/2.d150306f.chunk.js"
+  },
+  {
+    "revision": "c311b303e2b0c844647958548cbd7278",
+    "url": "/Todo/index.html"
+  }
+];
